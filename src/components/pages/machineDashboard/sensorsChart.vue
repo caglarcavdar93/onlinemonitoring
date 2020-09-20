a710649398120a8f748d8ec2db213772fcd5023f
Çağlar Çavdar <caglar.cavdar@windowslive.com>
2020-09-20 21:40:48 +0300

    chart config changed

--- src/components/pages/machineDashboard/sensorsChart.vue
+++ src/components/pages/machineDashboard/sensorsChart.vue
@@ -14,26 +14,7 @@ export default {
       chartLabel: "",
       chart: null,
       client: null,
-    };
-  },
-  methods: {
-    onConnect() {
-      this.client.subscribe("iotgateway/" + this.subscribedTopic);
-    },
-    onMessageArrived(message) {
-     try {
-        var data = JSON.parse(message.payloadString);
-      if (this.tagDataArr.length < 60) {
-        this.tagDataArr.push(data.values[0].v);
-        var dateTime= new Date(data.values[0].t).toISOString();
-        this.labelArr.push(dateTime);
-      }
-      var myCanvas = document.getElementById("myChart" + this.subscribedTopic);
-      var ctx = myCanvas.getContext("2d");
-      if (this.chart != null) {
-        this.chart.destroy();
-      }
-      this.chart = new Chart(ctx, {
+      chartConfig: {
         // The type of chart we want to create
         type: "line",
         // The data for our dataset
@@ -45,7 +26,7 @@ export default {
               backgroundColor: "rgb(255, 99, 132)",
               borderColor: "rgb(255, 99, 132)",
               fill: false,
-              data: this.tagDataArr
+              data: this.tagDataArr,
             },
           ],
         },
@@ -56,35 +37,56 @@ export default {
             duration: 0, // general animation time
           },
           scales: {
-            xAxes: [{
+            xAxes: [
+              {
                 ticks: {
-                    display: false //this will remove only the label
-                }
-            }]
-        },
-            // grid line settings
-            gridLines: {
-              drawOnChartArea: false, // only want the grid lines for one axis to show up
-            },
+                  display: false, //this will remove only the label
+                },
+              },
+            ],
           },
-        });
-        if(this.tagDataArr.length==30){
-          this.tagDataArr.shift();
-           this.labelArr.shift();
+          // grid line settings
+          gridLines: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
+      },
+    };
+  },
+  methods: {
+    onConnect() {
+      this.client.subscribe("iotgateway/" + this.subscribedTopic);
+    },
+    onMessageArrived(message) {
+      try {
+        var data = JSON.parse(message.payloadString);
+        if (this.tagDataArr.length < 60) {
+          this.tagDataArr.push(data.values[0].v);
+          var dateTime = new Date(data.values[0].t).toISOString();
+          this.labelArr.push(dateTime);
         }
-     } catch (error) {
-       console.log(error);
-     }
-     
+        this.chart.data.datasets[0].data = this.tagDataArr;
+        this.chart.data.labels = this.labelArr;
+        this.chart.update();
+        if (this.tagDataArr.length == 30) {
+          this.tagDataArr.shift();
+          this.labelArr.shift();
+        }
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
-  beforeMount: function () {
+  mounted: function () {
     this.client = new this.$Paho.Client("localhost", 9001, "");
     this.client.onMessageArrived = this.onMessageArrived;
     this.client.connect({
       cleanSession: true,
       onSuccess: this.onConnect,
     });
+    var myCanvas = document.getElementById("myChart" + this.subscribedTopic);
+    var ctx = myCanvas.getContext("2d");
+    this.chart = new Chart(ctx, this.chartConfig);
   },
   beforeDestroy: function () {
     this.client.disconnect();
